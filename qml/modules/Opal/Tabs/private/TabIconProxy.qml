bf7db07f1c0fc7c05593d481c47b366e65a9400c
roundedrectangle <roundedrectangle0@gmail.com>
2026-08-21 00:35:03 +0300

    Sync Opal Tabs with latest unminified source with pending PRs applied

---
+++ qml/modules/Opal/Tabs/private/TabIconProxy.qml
@@ -0,0 +1,11 @@
+//@ This file is part of Opal.Tabs.
+//@ SPDX-FileCopyrightText: 2026 roundedrectangle
+//@ SPDX-License-Identifier: GPL-3.0-or-later
+
+import QtQuick 2.0
+
+QtObject {
+    property size sourceSize
+    property color color
+    property color highlightColor
+}
--- qml/modules/Opal/Tabs/private/Util.js
+++ qml/modules/Opal/Tabs/private/Util.js
@@ -4,22 +4,45 @@
 //@ SPDX-License-Identifier: GPL-3.0-or-later
 //@ Original license: BSD-3-Clause (see separate license file SILICA-LICENSE)
 //@ Original copyright notices are listed above.
+
 .pragma library
-function findFlickable(item){var parentItem=item?item.parent:null
-while(parentItem){if(parentItem.maximumFlickVelocity&&!parentItem.hasOwnProperty("__silica_hidden_flickable")){return parentItem
-}parentItem=parentItem.parent
-}return null
+
+function findFlickable(item) {
+    var parentItem = item ? item.parent : null
+    while (parentItem) {
+        if (parentItem.maximumFlickVelocity && !parentItem.hasOwnProperty('__silica_hidden_flickable')) {
+            return parentItem
+        }
+        parentItem = parentItem.parent
+    }
+    return null
 }
-function findParentWithProperty(item,propertyName){var parentItem=item?item.parent:null
-while(parentItem){if(parentItem.hasOwnProperty(propertyName)){return parentItem
-}parentItem=parentItem.parent
-}return null
+
+function findParentWithProperty(item, propertyName) {
+    var parentItem = item ? item.parent : null
+    while (parentItem) {
+        if (parentItem.hasOwnProperty(propertyName)) {
+            return parentItem
+        }
+        parentItem = parentItem.parent
+    }
+    return null
 }
-function findPageStack(item){return findParentWithProperty(item,"_pageStackIndicator")
+
+function findPageStack(item) {
+    return findParentWithProperty(item, '_pageStackIndicator')
 }
-function findPage(item){return findParentWithProperty(item,"__silica_page")
+
+function findPage(item) {
+    return findParentWithProperty(item, '__silica_page')
 }
-function childAt(parent,x,y){var child=parent.childAt(x,y)
-if(child&&child.hasOwnProperty("fragmentShader")&&child.source&&child.source.layer.effect){child=child.source
-}return child
+
+function childAt(parent, x, y) {
+    var child = parent.childAt(x, y)
+    if (child && child.hasOwnProperty("fragmentShader") && child.source && child.source.layer.effect) {
+        // This item is a layer effect of the actual item.
+        child = child.source
+    }
+
+    return child
 }
