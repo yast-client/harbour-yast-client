8e913de12dc8c7161e022b29b8ba6d758a950d45
roundedrectangle <roundedrectangle0@gmail.com>
2026-09-11 22:22:59 +0300

    Fix debug module for lottie qt

--- src/harbour-yast-client.cpp
+++ src/harbour-yast-client.cpp
@@ -16,9 +16,9 @@
 // The default filter can be overridden by QT_LOGGING_RULES envinronment variable, e.g.
 // QT_LOGGING_RULES="yaqtlib.*=true;yast-client.*=true" harbour-yast-client
 #if defined (QT_DEBUG) || defined(DEBUG)
-#  define DEFAULT_LOG_FILTER "yaqtlib.*=true\nyast-client.*=true"
+#  define DEFAULT_LOG_FILTER "yaqtlib.*=true\nyast-client.*=true\nlottieqt.*=true"
 #else
-#  define DEFAULT_LOG_FILTER "yaqtlib.*=false\nyast-client.*=false"
+#  define DEFAULT_LOG_FILTER "yaqtlib.*=false\nyast-client.*=false\nlottieqt.*=false"
 #endif
 
 #define JS_DEBUG_ROOT_MODULE "yast-client.JS"
